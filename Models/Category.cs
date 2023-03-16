383ebad28b4ef2dd1d1f8e143c2c853648b7d540
Shahab Matapour <shahab1242000@yahoo.com>
2023-03-16 20:02:08 +0100

    Sample source

---
+++ Models/Category.cs
@@ -0,0 +1,11 @@
+﻿
+namespace HPlusSport.API.Models
+{
+    public class Category
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public virtual List<Product> Products { get; set; }
+
+    }
+}
---
+++ Models/ShopContext.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Components.Server.ProtectedBrowserStorage;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+
+namespace HPlusSport.API.Models
+{
+    public class ShopContext : DbContext
+    {
+        public ShopContext(DbContextOptions<ShopContext> options) : base(options)
+        {
+        }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Category>()
+                .HasMany(c => c.Products)
+                .WithOne(a => a.Category)
+                .HasForeignKey(a => a.CategoryId);
+            modelBuilder.Seed();
+
+        }
+        public DbSet<Product> Products { get; set;}
+        public DbSet<Category> Categories { get; set;}
+
+    }
+}
